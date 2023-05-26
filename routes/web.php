2085d0c061bfc4e8d6f8f3b03bcd60b6c5f6f048
divzpu <zahraisyapu@gmail.com>
2023-05-26 21:07:08 +0700

    al ini udah bener fix

--- routes/web.php
+++ routes/web.php
@@ -26,7 +26,7 @@ Route::get('/login', function () {
     return view('login');
 });
 
-Route::get('/register', function () {
-    return view('register');
+Route::get('/registerfr', function () {
+    return view('registerfr');
 });
 
